5c4e27d32049bf7f7256859d6bb4f7cfca3ccc31
drlsxs <15680262661@163.com>
2021-08-24 17:10:50 +0800

    修改为查看详细;新增改为ip + 端口号;数据项改为节点;根据节点状态动态改变显示节点的状态;主节点，子节点名字改一下;进入页面加上标题，点击返回主页

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="Y_container">
+    <div data-v-fae5bece=""  class="headlines childE" style="transform: scale(0.594271); z-index: 1999" @click="home">
+      <h3 data-v-fae5bece="">
+        <a href="/" style="color: #09C8E7">星杓大数据平台</a>
+      </h3>
+    </div>
     <div class="Y-center">
       <div class="warp" ref="warp">
         <!--底部方块 开始-->
@@ -75,7 +80,7 @@
                   <div class="do-top">
                     <div class="xz-light"></div>
                   </div>
-                  <div class="do-anim">
+                  <div class="do-anim" :class="'doAnim'+item.status">
                     <span class="an-guang"></span>
                     <span class="an-guang-sp">
                 <img src="@/assets/outside2/images/sjy_top03.png">
@@ -117,7 +122,6 @@
               <!--节点容器结束-->
             </el-col>
           </el-row>
-
         </div>
 
         <!--至于箭头,可以这么做先定义箭头开始：-->
@@ -144,8 +148,11 @@
       >
         <div class="demo-drawer__content">
           <el-form ref="form" :model="form" label-width="90px" :title='formTitle'>
-            <el-form-item label="url" prop="url">
-              <el-input v-model="form.url" placeholder="请输入url"></el-input>
+            <el-form-item label="url" prop="ip">
+              <el-input v-model="form.url" placeholder="请输入ip"></el-input>
+            </el-form-item>
+            <el-form-item label="端口号" prop="port">
+              <el-input v-model="form.url" placeholder="请输入端口号"></el-input>
             </el-form-item>
           </el-form>
           <div class="demo-drawer__footer">
@@ -258,7 +265,7 @@
             <div class="el-message-box__content">
               <div class="el-message-box__container"><!---->
                 <div class="el-message-box__status el-icon-warning"></div>
-                <div class="el-message-box__message"><p><span>{{`是否确认删除url为${deleteUrl}的数据项?`}} </span></p></div>
+                <div class="el-message-box__message"><p><span>{{`是否确认删除url为${deleteUrl}的节点?`}} </span></p></div>
               </div>
               <div class="el-message-box__input" style="display: none;">
                 <div class="el-input"><!----><input type="text" autocomplete="off" placeholder="" class="el-input__inner"><!----><!---->
@@ -299,7 +306,7 @@
 import '@/assets/outside2/js/eui'
 import '@/assets/outside2/js/ping'
 import '@/assets/outside2/js/iconfont'
-import { listType,getType,delType,updateType,addType,selectList,insertList,deleteList,updateList } from "@/api/index";
+import { listType,getType,delType,updateType,addType } from "@/api/index";
 export default {
   name: "Center",
   mounted() {
@@ -351,10 +358,15 @@ export default {
         extension_version: "1.0.1.1",
         extension_id: "test4",
         slave_id: "test1"
-      }
+      },
     };
   },
   methods: {
+    // 返回主页
+    home() {
+      console.log(23232);
+      this.$router.push({path: '/'})
+    },
     //关闭详情抽屉
     handleCloseDetail() {
       this.drawerDetail = false;
@@ -388,62 +400,12 @@ export default {
         }
       );
     },
-    selectData() {
-      this.type = "select"
-      var data = {
-        type: this.type,
-        tname: this.tname,
-        page: this.page,
-        rows: this.rows
-      };
-      selectList(data).then(response => {
-        console.log(response, 88777);
-      });
-    },
-    insertData() {
-      this.type = "insert"
-      var data = {
-        type: this.type,
-        tname: this.tname,
-        values: this.values
-      };
-      insertList(data).then(response => {
-        console.log(response, 88777);
-      });
-    },
-    deleteData() {
-      this.type = "delete";
-      var data = {
-        type: this.type,
-        tname: this.tname,
-        conditions: this.conditions
-      };
-      deleteList(data).then(response => {
-        console.log(response, 88777);
-      });
-    },
-    updateData() {
-      this.type = "update";
-      var data = {
-        type: this.type,
-        tname: this.tname,
-        values: this.values,
-        //fixme 已写死
-        conditions: {
-          extension_id: "test3"
-        },
-      };
-      updateList(data).then(response => {
-        console.log(response, 88777);
-      });
-    },
     submitForm: function() {
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.id !== undefined) {  // 修改
             var data = {
               id:this.id,
-              //fixme 保证ip前后没有空格
               url:this.form.url.trim()
             }
             updateType(data).then(response => {
@@ -503,7 +465,7 @@ export default {
     //查看详情
     handleDetail(item) {
       this.drawerDetail = true;
-      this.drawerTitle = '查看 '+item.url+' 的详情'
+      this.drawerTitle = '节点详情'
       console.log(item);
       var data = {
         id:item.uuid,
@@ -548,10 +510,6 @@ export default {
   },
   created() {
     this.getList();
-    // this.selectData();
-    // this.insertData();
-    // this.deleteData();
-    // this.updateData();
   },
   computed: {
     //计算属性计算出当前的order值
@@ -568,6 +526,33 @@ export default {
 <style scoped src="@/assets/outside2/css/indexcontent.css"></style>
 <style scoped src="@/assets/css/style.css"></style>
 <style scoped>
+/*控制节点的颜色*/
+.doAnim0 {
+  background: url(../assets/outside2/images/sjy_top020.png) bottom no-repeat;
+}
+
+.doAnim0 .an-guang {
+  background: url(../assets/outside2/images/sjy_light0.png) no-repeat;
+}
+
+.doAnim0 .an-jiaz {
+  background: url(../assets/outside2/images/sjy_top040.png) no-repeat;
+}
+
+.doAnim1 {
+  background: url(../assets/outside2/images/sjy_top021.png) bottom no-repeat;
+}
+
+.doAnim1 .an-guang {
+  background: url(../assets/outside2/images/sjy_light1.png) no-repeat;
+}
+
+.doAnim1 .an-jiaz {
+  background: url(../assets/outside2/images/sjy_top041.png) no-repeat;
+}
+
+
+
 .Y_container {
   zoom: 1;background: #162878\0;background-image: linear-gradient(180deg, #193b8f 0%, #110450 100%);
 }
@@ -582,7 +567,6 @@ export default {
 .node-container {
   width: 100%;
   position: absolute;
-  /*fixme 高度以写死*/
   left: 0;
   top: -50px;
   transform: translate(-50%,-50%);
@@ -752,16 +736,25 @@ img.h2 {
   background: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(../assets/images/kaibg.png) no-repeat;
   background-size: cover;
 }
+.el-drawer__header {
+  color: #fff !important;
+}
+.el-drawer__close-btn {
+  color: #72767b !important;
+}
+.el-form-item__label {
+  color: #fff !important;
+}
 .demo-drawer__content {
   display: flex;
   flex-direction: column;
   height: 100%;
   width: 100%;
-  color: #fff;
   box-sizing: border-box;
   border: 2px solid #016ae0;
   border-radius: 10px;
   overflow: hidden;
+
 }
 .addDrawer .demo-drawer__content,.delDrawer .demo-drawer__content {
   border: none;
@@ -899,5 +892,8 @@ img.h2 {
 .demo-drawer__footer .el-button:nth-child(2) {
   background-color: #00A0CC;
 
+}
+.el-form-item {
+  margin-bottom: 20px !important;
 }
 </style>
